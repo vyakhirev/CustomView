6e6c142a723bcd5e5cd613adcdca0e263b155021
Mikhail U. Vyakhirev <vmu-ace@.ya.ru>
2019-11-26 23:38:02 +0300

    Legend badly made added

--- app/src/main/java/ru/vyakhirev_m/customview/PieChartWithLegent.kt
+++ app/src/main/java/ru/vyakhirev_m/customview/PieChartWithLegent.kt
@@ -18,20 +18,22 @@ class PieChart : View {
 //
     private val paint= Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private val rectPadding=25f
+    private val padding=25f
 
 
     private val bgColor=Color.rgb(46,51,89)
     private val firstColor=Color.rgb(0,190,224)
     private val secondColor=Color.rgb(255,225,0)
     private val thirdColor=Color.rgb(180,152,207)
+    private val legendColorDark=Color.rgb(0,0,0)
+    private val legendColorLight=Color.rgb(255,255,255)
 
     var rightCorner:Float=0f
-    lateinit var rectF:RectF
+    lateinit var rectPie:RectF
 
-    private var maxValue : Int = 0
-    private var widthPerView = 0
-    private var heightPerValue = 0
+//    private var maxValue : Int = 0
+//    private var widthPerView = 0
+//    private var heightPerValue = 0
 
     fun PercentCount(Clients:List<Client>,pos:Int=0):Float {
         var totalAmount=0f
@@ -42,11 +44,12 @@ class PieChart : View {
     var secondItemAngel=PercentCount(Clients,1)
     var thirdItemAngel=PercentCount(Clients,2)
 
+
     //Draw rectagle vdol ili poperek
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val desiredWidth = 600
-        val desiredHeight = 600
+        val desiredHeight = 900
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
@@ -85,36 +88,53 @@ class PieChart : View {
         canvas.drawColor(bgColor)
         paint.color=firstColor
         rightCorner= Math.min(height,width).toFloat()
-        rectF= RectF(rectPadding,rectPadding,rightCorner-rectPadding, rightCorner-rectPadding)
-        canvas.drawArc(rectF,0f,firstItemAngel,true,paint)
+        rectPie= RectF(padding,padding,rightCorner-padding, rightCorner-padding)
+        canvas.drawArc(rectPie,0f,firstItemAngel,true,paint)
 //        canvas.drawRoundRect(rectPadding,)
         paint.color=secondColor
-        canvas.drawArc(rectF,firstItemAngel,secondItemAngel,true,paint)
+        canvas.drawArc(rectPie,firstItemAngel,secondItemAngel,true,paint)
         paint.color=thirdColor
-        canvas.drawArc(rectF,secondItemAngel+firstItemAngel,thirdItemAngel,true,paint)
-//        canvas.drawRect(0f,100f,width+200f,height+200f,paint)
+        canvas.drawArc(rectPie,secondItemAngel+firstItemAngel,thirdItemAngel,true,paint)
+//Draw first legend
+        paint.color=firstColor
+        canvas.drawRoundRect(padding,601f,width*0.85f,670f,20f,25f,paint)
+        paint.color=legendColorLight
+        paint.textSize= 50F
+        canvas.drawText(Clients[0].name+" "+Clients[0].pays.toString(),1.5f*padding,650f,paint)
+//Draw second legend
+        paint.color=secondColor
+        canvas.drawRoundRect(padding,690f,width*0.85f,760f,20f,25f,paint)
+        paint.color=legendColorDark
+        paint.textSize= 50F
+        canvas.drawText(Clients[1].name+" "+Clients[1].pays.toString(),1.5f*padding,740f,paint)
+//Draw first legend
+        paint.color=thirdColor
+        canvas.drawRoundRect(padding,780f,width*0.85f,850f,20f,25f,paint)
+        paint.color=legendColorLight
+        paint.textSize= 50F
+        canvas.drawText(Clients[2].name+" "+Clients[2].pays.toString(),1.5f*padding,830f,paint)
     }
 }
 
-class Legend:View{
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
-    private val paint= Paint(Paint.ANTI_ALIAS_FLAG)
-
-    private val rectPadding=25f
-
-
-    private val bgColor=Color.rgb(46,51,89)
-    private val firstColor=Color.rgb(0,190,224)
-    private val secondColor=Color.rgb(255,225,0)
-    private val thirdColor=Color.rgb(180,152,207)
-
-
-}
+//class Legend:View{
+//    constructor(context: Context?) : super(context)
+//    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+//    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+//
+//    private val paint= Paint(Paint.ANTI_ALIAS_FLAG)
+//
+//    private val rectPadding=25f
+//
+//
+//    private val bgColor=Color.rgb(46,51,89)
+//    private val firstColor=Color.rgb(0,190,224)
+//    private val secondColor=Color.rgb(255,225,0)
+//    private val thirdColor=Color.rgb(180,152,207)
+//
+//
+//}
 data class Client(val name:String="Kan",var trenings:Int,var pays:Int)
 val Clients= listOf<Client>(
     Client("Иванов",15,15000),
     Client("Петров",6,19000),
-    Client("Сидоров",17,18000))
+    Client("Сидорренко",17,18000))
